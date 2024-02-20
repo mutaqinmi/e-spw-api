e0dd4960a87ea6aff0389213f25ebb2070a902fc
mutaqinmi <mutaqinmi2586@gmail.com>
2024-02-20 15:06:10 +0700

    feat: add customization on navbar

--- public/assets/script/index.js
+++ public/assets/script/index.js
@@ -1,16 +1,37 @@
 $(() => {
+    const bodyWidth = window.matchMedia("(max-width: 768px)").matches;
     const menuButton = $(".menu-button");
     const menuLink = $(".menu-link");
+    const featureButton = $(".feature-button");
+    const featureSubMenu = $(".sub-menu");
     const garis1 = $(".garis1");
     const garis2 = $(".garis2");
     const garis3 = $(".garis3");
+    
+    if(!bodyWidth){
+        menuLink.removeClass("hide");
+        garis2.removeClass("hide")
+        garis1.removeClass("rotate-right")
+        garis3.removeClass("rotate-left")
+    } else {
+        menuLink.addClass("hide");
+        featureButton.on("click", () => {
+            featureSubMenu.toggleClass("hide");
+        })
+    }
     
     $(window).on("resize", () => {
         const bodyWidth = window.matchMedia("(max-width: 768px)").matches;
         if(!bodyWidth){
             menuLink.removeClass("hide");
+            garis2.removeClass("hide")
+            garis1.removeClass("rotate-right")
+            garis3.removeClass("rotate-left")
         } else {
             menuLink.addClass("hide");
+            featureButton.on("click", () => {
+                featureSubMenu.toggleClass("hide");
+            })
         }
     })
 
