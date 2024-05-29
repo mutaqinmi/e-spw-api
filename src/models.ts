fe27bea8982246e7b6f5da945398588a3195b9d8
mutaqinmi <mutaqinmi2586@gmail.com>
2024-05-29 11:37:20 +0700

    fix: undefined id_produk

--- src/models.ts
+++ src/models.ts
@@ -89,9 +89,12 @@ export const cariProduk = async (keywords: string) : Promise<Array<any>> => {
 
 export const addProduk = async (nama_produk: string, harga: string, stok: number, deskripsi_produk: string, detail_produk: string, id_toko: string) => {
     const product: {[key: string]: any} = await db.select().from(table.produk).where(eq(table.produk.id_toko, id_toko));
-    const increment: number = await product?.length + parseInt(await product[0]?.['id_produk'].split('-')[2]) + 1;
+    let increment: number = 1;
+    if(product){
+        increment = product?.length + parseInt(product[0]?.['id_produk'].split('-')[2]) + 1;
+    }
 
-    return db.insert(table.produk).values({
+    return await db.insert(table.produk).values({
         "id_produk": `product-${id_toko.split('-')[1]}-${increment}`,
         "id_toko": id_toko,
         "nama_produk": nama_produk,
