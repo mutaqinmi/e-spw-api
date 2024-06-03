f757e2402f36757e39a889b6f97da4dc9a4c57a9
mutaqinmi <mutaqinmi2586@gmail.com>
2024-06-03 18:22:44 +0700

    feat: transaksi join ulasan

--- src/models.ts
+++ src/models.ts
@@ -205,11 +205,11 @@ export const getNotifikasi = async (nis: number, type: string) : Promise<Array<a
 }
 
 export const getRiwayatUlasan = async (nis: number) : Promise<Array<any>> => {
-    return await db.select().from(table.ulasan).leftJoin(table.produk, eq(table.ulasan.id_produk, table.produk.id_produk)).leftJoin(table.toko, eq(table.produk.id_toko, table.toko.id_toko)).where(eq(table.ulasan.nis, nis));
+    return await db.select().from(table.ulasan).leftJoin(table.transaksi, eq(table.ulasan.id_transaksi, table.transaksi.id_transaksi)).leftJoin(table.produk, eq(table.ulasan.id_produk, table.produk.id_produk)).leftJoin(table.toko, eq(table.produk.id_toko, table.toko.id_toko)).where(eq(table.ulasan.nis, nis));
 }
 
 export const getRiwayatUlasanByShop = async (id_produk: string) : Promise<Array<any>> => {
-    return await db.select().from(table.ulasan).leftJoin(table.produk, eq(table.ulasan.id_produk, table.produk.id_produk)).leftJoin(table.toko, eq(table.produk.id_toko, table.toko.id_toko)).where(eq(table.ulasan.id_produk, id_produk));
+    return await db.select().from(table.ulasan).leftJoin(table.transaksi, eq(table.ulasan.id_transaksi, table.transaksi.id_transaksi)).leftJoin(table.produk, eq(table.ulasan.id_produk, table.produk.id_produk)).leftJoin(table.toko, eq(table.produk.id_toko, table.toko.id_toko)).where(eq(table.ulasan.id_produk, id_produk));
 }
 
 export const addUlasan = async (nis: number, id_produk: string, id_transaksi: string, deskripsi_ulasan: string, jumlah_rating: string) => {
