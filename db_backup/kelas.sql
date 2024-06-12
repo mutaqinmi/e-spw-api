8042af98d36268b032b24d965ce4bb2470b240d9
mutaqinmi <mutaqinmi2586@gmail.com>
2024-06-12 14:51:09 +0700

    fix: user sql

--- db_backup/kelas.sql
+++ db_backup/kelas.sql
@@ -1,10 +1,10 @@
 INSERT INTO kelas VALUES
-                    ('tjkt12023', 'X TJKT 1', 'Teknik Jaringan Komputer dan Telekomunikasi', '108004122009022005'),
-                    ('tjkt22023', 'X TJKT 2', 'Teknik Jaringan Komputer dan Telekomunikasi', '108004122009022005'),
-                    ('pplg2023', 'X PPLG', 'Pengembangan Perangkat Lunak dan Gim', '108004122009022005'),
-                    ('tjkt12022', 'XI TJKT 1', 'Teknik Jaringan Komputer dan Telekomunikasi', '108004122009022005'),
-                    ('tjkt22022', 'XI TJKT 2', 'Teknik Jaringan Komputer dan Telekomunikasi', '108004122009022005'),
-                    ('pplg2022', 'XI PPLG', 'Pengembangan Perangkat Lunak dan Gim', '108004122009022005'),
-                    ('tjkt12021', 'XII TJKT 1', 'Teknik Jaringan Komputer dan Telekomunikasi', '108004122009022005'),
-                    ('tjkt22021', 'XII TJKT 2', 'Teknik Jaringan Komputer dan Telekomunikasi', '108004122009022005'),
-                    ('pplg2021', 'XII PPLG', 'Pengembangan Perangkat Lunak dan Gim', '108004122009022005');
+                    ('tjkt12023', 'X TJKT 1', 'Teknik Jaringan Komputer dan Telekomunikasi', '198004122009022005'),
+                    ('tjkt22023', 'X TJKT 2', 'Teknik Jaringan Komputer dan Telekomunikasi', '198004122009022005'),
+                    ('pplg2023', 'X PPLG', 'Pengembangan Perangkat Lunak dan Gim', '198004122009022005'),
+                    ('tjkt12022', 'XI TJKT 1', 'Teknik Jaringan Komputer dan Telekomunikasi', '198004122009022005'),
+                    ('tjkt22022', 'XI TJKT 2', 'Teknik Jaringan Komputer dan Telekomunikasi', '198004122009022005'),
+                    ('pplg2022', 'XI PPLG', 'Pengembangan Perangkat Lunak dan Gim', '198004122009022005'),
+                    ('tjkt12021', 'XII TJKT 1', 'Teknik Jaringan Komputer dan Telekomunikasi', '198004122009022005'),
+                    ('tjkt22021', 'XII TJKT 2', 'Teknik Jaringan Komputer dan Telekomunikasi', '198004122009022005'),
+                    ('pplg2021', 'XII PPLG', 'Pengembangan Perangkat Lunak dan Gim', '198004122009022005');
