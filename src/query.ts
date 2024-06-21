1815c9a38b58d27002b9e1bc5bf4aa636f3b0425
mutaqinmi <mutaqinmi2586@gmail.com>
2024-06-21 16:16:49 +0700

    fix: add product

--- src/query.ts
+++ src/query.ts
@@ -136,7 +136,9 @@ export const addProduk = async (nama_produk: string, harga: string, stok: number
     const product = await db.select().from(table.produk).where(eq(table.produk.id_toko, id_toko));
     let increment: number = 1;
     if(product){
-        increment = product.length + parseInt(product[0]['id_produk'].split('-')[2]) ?? 0 + 1;
+        if(product.length != 0){
+            increment = product.length + parseInt(product[0]['id_produk'].split('-')[2]) ?? 0 + 1;
+        }
     }
 
     return await db.insert(table.produk).values({
